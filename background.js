00c5ef36d5c370038781ed33984ef31bff84e50a
Anushka7310 <anushkaguptably@gmail.com>
2022-10-17 22:23:35 +0530

    added bookmark

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  if (tab.url && tab.url.includes("youtube.com/watch")){
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    //we are going to send A message to content script that basically says a new video is loaded.
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+
+    });
+  }
+})
